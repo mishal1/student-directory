bc8dc4915203360c234f95596956841a938b6a39
Mishal Islam <mishal.islam01@gmail.com>
2014-10-14 17:26:56 +0100

    refactored students

--- directory.rb
+++ directory.rb
@@ -1,11 +1,15 @@
+students =["Dr. Hannibal Lecter",
+"Darth Vader",
+"Nurse Ratched",
+"Michael Corleone",
+"Alex De Large",
+"The Alien",
+"Terminator",
+"Freddy Kruger",
+"The Joker"]
+
 puts "The students of my cohort at Makers Academy"
-puts "-------------"
-puts "Dr. Hannibal Lecter"
-puts "Darth Vader"
-puts "Nurse Ratched"
-puts "Michael Corleone"
-puts "Alex De Large"
-puts "The Alien"
-puts "Terminator"
-puts "Freddy Kruger"
-puts "The Joker"
+students.each {|name| puts name}
+
+
+puts "Overall, we have #{students.length} great students"
