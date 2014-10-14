242acc1ae6d7fe31c5f9cd9742460930262914f8
Mishal Islam <mishal.islam01@gmail.com>
2014-10-14 17:32:12 +0100

    Added methods

--- directory.rb
+++ directory.rb
@@ -8,8 +8,19 @@ students =["Dr. Hannibal Lecter",
 "Freddy Kruger",
 "The Joker"]
 
+def print_header
 puts "The students of my cohort at Makers Academy"
-students.each {|name| puts name}
+puts "-----------"
+end
 
+def print(names)
+names.each {|name| puts name}
+end
 
-puts "Overall, we have #{students.length} great students"
+def print_footer(names)
+puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
