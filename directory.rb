ee29e7f5664a12b5c334c395eedc634de5af72f9
Mishal Islam <mishal.islam01@gmail.com>
2014-10-15 16:55:15 +0100

    Aligned text

--- directory.rb
+++ directory.rb
@@ -15,35 +15,43 @@ students =[
 
 =end
 
-
 def print_header
-puts "The students of my cohort at Makers Academy"
-puts "-----------"
+	width =50
+	puts "The students of my cohort at Makers Academy".center(width)
+	puts "-----------".center(width)
 end
 
 def print(students)
-
-students.each do |student| 
+	width =50
+	students.each do |student| 
 	until
-		puts "#{student[:name]} (#{student[:cohort]}) cohort"
+		puts "#{student[:name]} (#{student[:cohort]}) cohort".center(width)
 		break
 	end
 end
 end
 
 def print_footer(names)
-puts "Overall, we have #{names.length} great students"
+	width =50
+	puts "Overall, we have #{names.length} great students".center(width)
 end
 
 def input_students
-	puts "Please enter the name of the students"
-	puts "To finish, just hit return twice"
+	width =50
+	puts "Please enter the name of the students".center(width)
+	puts "To finish, just hit return twice".center(width)
 	students = []
 	name = gets.chomp
 	name = name.capitalize
-	while !name.empty? do 
-		students << {:name => name, :cohort => :november}
-		puts "Now we have #{students.length} students"
+	puts "What are the student's hobbies?".center(width)
+	hobbies = gets.chomp
+	puts "Where is the student's country of birth?".center(width)
+	origin = gets.chomp
+	puts "What is the students height?".center(width)
+	height = gets.chomp
+	while !name.empty? do
+		students << {:name => name, :cohort => :november, :hobbies => hobbies, :origin=>origin, :height=> height}
+		puts "Now we have #{students.length} students".center(width)
 		name = gets.chomp
 		name = name.capitalize
 	end
